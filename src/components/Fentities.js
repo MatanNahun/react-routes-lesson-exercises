3bdee60741960026b9c6eea5c03ac6d861b2d952
MatanNahum <matan.nahum95@gmail.com>
2022-11-08 12:05:58 +0200

    before exercises

--- src/components/Fentities.js
+++ src/components/Fentities.js
@@ -1,24 +1,27 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
-import '../styles/fentity-directory.css'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "../styles/fentity-directory.css";
 
 class Fentities extends Component {
-    render() {
-        return (
-            <div>
-                <h1 id="fentities-title">{/*Get from `match`*/}</h1>
-                <div id="fentities-container">
-                    {fentities.map(f => {
-                        return (
-                            <div className="mini">
-                                <img className="directory-img" src={f.imgUrl} alt="" />
-                                <span>{f.name}</span>
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>)
-    }
+  render() {
+    console.log(this.props.match.params.fentities);
+    let fentitiesCategory = this.props.match.params.fentities;
+    return <div>{this.props.match.params.fentities}</div>;
+    //  (
+    //     <div>
+    //         <h1 id="fentities-title">{/*Get from `match`*/}</h1>
+    //         <div id="fentities-container">
+    //             {fentities.map(f => {
+    //                 return (
+    //                     <div className="mini">
+    //                         <img className="directory-img" src={f.imgUrl} alt="" />
+    //                         <span>{f.name}</span>
+    //                     </div>
+    //                 )
+    //             })}
+    //         </div>
+    //     </div>)
+  }
 }
 
-export default Fentities
+export default Fentities;
